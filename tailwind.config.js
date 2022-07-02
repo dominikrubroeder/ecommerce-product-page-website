aa8bcaedcb3c84e0754602bc4c706a383a82782a
Dominik <dominik.rubroeder@icloud.com>
2022-07-02 15:42:46 +0200

    Add project colors and documentation files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'app-primary-orange': 'hsl(26, 100%, 55%)',
+        'app-primary-orange-pale': 'hsl(25, 100%, 94%)',
+        'app-neutral-blue': 'hsl(220, 13%, 13%)',
+        'app-neutral-blue-grayish': 'hsl(219, 9%, 45%)',
+        'app-neutral-blue-grayish-dark': 'hsl(220, 14%, 75%)',
+        'app-neutral-blue-grayish-light': 'hsl(223, 64%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
